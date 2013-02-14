107e23598ab55a0a30ea854d41951b2d536349ab
Austin Hastings <Austin_Hastings@yahoo.com>
2013-02-14 01:42:35 -0500

    checked in

---
+++ ch01/cradle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf8
+"""
+    ch01.cradle
+    ~~~~~~~~~~~
+
+    A framework of commonly-used routines for the Let's Build a Compiler (in Python)!
+    project.
+
+    :copyright: 2013 by Austin Hastings, see AUTHORS for more details.
+    :license: GPL v3+, see LICENSE for more details.
+"""
+import sys
+import io
+import pdb
+
+##### Error handling
+
+def abort(msg):
+    pass
+
+def error(msg):
+    pass
+
+def expected(what):
+    pass
+
+##### Input handling
+
+def get_char():
+    pass
+
+def get_number():
+    pass
+
+def get_word():
+    pass
+
+def match(ch):
+    pass
+
+##### Output functions
+
+def emit(text):
+    pass
+
+def emitln(text):
+    pass
+
+##### Processing
+
+def init(in=None):
+    pass
+
+def main():
+    print("Enter you code on a single line. Enter '.' by itself to quit.")
+    while True:
+        line = input()
+        if line == ".":
+            break
+        init(in=StringIO(line))
+        compile()
+
+if __name__ == '__main__':
+    main()
+
---
+++ ch01/tests/cradle_tests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf8
+"""
+    ch01.tests.cradle_tests.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Specifies the behavior of the cradle functions.
+
+    :copyright: 2013 by Austin Hastings, see AUTHORS for more details.
+    :license: GPL v3+, see LICENSE for more details.
+"""
+import unittest
+
+from ch01 import cradle
+
+class TestCradle(unittest.TestCase):
+    
+    def test_abort():
+
+
+if __name__ == '__main__':
+    unittest.main()
+
